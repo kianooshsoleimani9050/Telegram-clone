e2b9b7f04073cde725ae32f34392e3070aea69e1
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2022-01-28 22:10:20 +0330

    feat: :zap: Add redux and redux persist

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/sass/index.scss";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );

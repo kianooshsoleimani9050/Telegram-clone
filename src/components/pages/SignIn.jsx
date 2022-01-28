cb68cad28c66faf050be5e3127657ffb1cd1a351
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2022-01-28 22:15:54 +0330

    refactor: remove tmpl defaulties

--- src/components/pages/SignIn.jsx
+++
@@ -1,9 +0,0 @@
-const SignIn = () => {
-  return (
-    <div className="p-0 m-0 w-100 text-center">
-      <h1 className="text-primary">Sign in page</h1>
-    </div>
-  );
-};
-
-export default SignIn;

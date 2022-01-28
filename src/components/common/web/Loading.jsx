45ae50d80c213aafbb56dc0df10fbaa298005473
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2022-01-28 22:15:14 +0330

    feat: :sparkles: A loading component

---
+++ src/components/common/web/Loading.jsx
@@ -0,0 +1,15 @@
+const Loading = () => {
+  return (
+    <div className="p-0 m-0 d-flex align-items-center justify-content-center">
+      <div className="p-0 m-2 loading_line_div__container" />
+      <div className="p-0 m-2 loading_line_div__container" />
+      <div className="p-0 m-2 loading_line_div__container" />
+      <div className="p-0 m-2 loading_line_div__container" />
+      <div className="p-0 m-2 loading_line_div__container" />
+      <div className="p-0 m-2 loading_line_div__container" />
+      <div className="p-0 m-2 loading_line_div__container" />
+    </div>
+  );
+};
+
+export default Loading;

0bdb5d4a3f8a09f7801750eb1aa1904588b8c0a2
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2022-01-28 22:15:31 +0330

    feat: :sparkles: A Home component

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,20 @@
+import { useSelector } from "react-redux";
+import { Outlet } from "react-router-dom";
+import Sidebar from "../common/layout/Sidebar";
 const Home = () => {
+  const showChats = useSelector((state) => state.showChats);
   return (
-    <div className="p-0 m-0 w-100 text-center">
-      <h1 className="text-primary">Home page</h1>
+    <div className="p-0 d-block d-md-flex home_div__container">
+      <div className="p-0 m-0 home_navbar_div__container">
+        <Sidebar />
+      </div>
+      <div
+        className={`p-0 m-0 home_outlet_div__conatiner ${
+          showChats ? "open" : "close"
+        }`}
+      >
+        <Outlet />
+      </div>
     </div>
   );
 };
